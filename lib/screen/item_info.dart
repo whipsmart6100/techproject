479b703147fd0450992f8f9185182bc3bcbdf519
artblitz <134607254+artblitz@users.noreply.github.com>
2023-12-18 17:30:04 +0530

    Removing Credentials

--- lib/screen/item_info.dart
+++ lib/screen/item_info.dart
@@ -107,22 +107,10 @@ class _ItemInfoState extends State<ItemInfo> {
     _getItem() async {
 
      // if(gotIt==0){
-      const cred = r'''
-      {
-  "type": "service_account",
-  "project_id": "whip-smart",
-  "private_key_id": "42eb00124df842e60933e5ce772a51e9c3209af5",
-  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQD1/sh+zyoSPdOJ\nqf83kZweW4K/0H8i2+cb0pJbLVqxIQ1z8zCckVNCpTE1j4kFTa5Jal9g2B6XIHZN\nEY5G21VeuRhDs+OmVF4DOVSHCsIpfV/hu3SCUgEibxXQxUsdK25umguVTsfBVSHk\nIdP+WoLwmKM61mDfgqLE3RI3TlmEyWDzOvOZPGKyGoe0Y01Y6H4uXwJpah5QPz7C\n94fX+f4GtN/9NLRqeqWVSbfgdhSK/+LZalI/kEutPEzkLm7TMlP7RHgYSzoaLoa1\nLD1zKPoPJ6U2VFulMRYXPuJm6OAHwJr2qWVvOTnZ2AN2QtfTNy5I+zrEWzsDTT7R\noSUIIpmZAgMBAAECggEACKGZcFWdgMlZkEV+Nnlt9GS6mDddFqCHoeLIDtnvCjMC\nMVTRmlTBCqAYM2xnXt/EY0APu6qpnBzf+cFJcDQG8U1mTA3WSYJBUDSlQh2/BUyo\nXhfBNUdiUc57AfnCMnJRLCFMkb8TZDWz/DixowWS/ZUleQ2HlaAUTPsCxh0CKBYF\nLA7imDemAlqhfTxqxTO/ytG5k/APsGSmC/JK6rUp0dnwS7jjYfo4K1KtF6/bQm0q\n0EWtwzYa1fJeBee+F8Iwd5KmOFrnIOzRc0lp8qmUbbBU9XoKtP+WVwSAjC+E9rF0\n6qSOHXgZ+JxbBIJmTsyj1I/po3WkMWANaaOlA/YbOQKBgQD74XdzvDTHlU8j3b2a\nT0HqvxuQgBAPGXmAyN6AmxJNDsKUpWbvb19uYMS8+RKXJfQix+QkTY8DmIsQOozk\nYw+4q6UUN0n5nRTLAX43snWIi1VcRbDc3zVqKHtJ+FeeIDBXz86kQJaQ8KtHm2Cb\naoC1fKP1wwVovPxg0NIzlyxVtQKBgQD6BK0a/QJ1pm/It4qXFLnH3N1P/m7dHwWY\ngRn9YqLxXtdH37cod8htMd1iZiRRHDCEg7FCf2gzFvO0FnsTtLziB/hA3Zh+WdC3\nLm9DQNZHdWAZ3ow1A4PYLJGoRntPIQSWoxmupKzaX2zKAj9h9dPZIWytjpR7fgIu\neOztz7Bi1QKBgG3O1n+rLitQaWcOdWk/YlJPrFmrqtSaW4EMkQvqDulvJN8e2KhL\nmoQJRdvIjhU845kCIfC9qzp7Fy0lCWVL1n/AvL/6dpKOq5Dw/rCTaW/0pmsKqtcv\nrB6ytUUDMldqWwj9PG4ZBkr70P/vugcnQCNeyjALLGGD+lbO0sTRA+/9AoGBAPFS\n9iuor2kUH63jy3gM303VGCGQez4gJWyQoJ99R74nRzDu9YGwLNuJWCEUalWIGiSu\nYLjI9bDqpIzdbisVN4QNIUirG/hJhTZrMaX+vVX4K6yMrVS2cdqFGosMjRVRz3X/\nNFjoRpf5p65NARTzDWr14kqb5yJT/JMS0c6xu5WJAoGAGRd8tSLqCbeS+K5SiAD8\nPnqxVQwTrk/qFKVI8/QVb8y5fVRtH5u6gZMcYUJvx6iRsMGZRCLSg1KFkkpVRQ4O\nDWdMrjp+xZKfGtrGKRHI3moTj+ChUHLXV1OjG68zy1duCzIGqTRvsV4HqIsF0uQ0\nrzIhErjnWSwwBOnUmtMetT0=\n-----END PRIVATE KEY-----\n",
-  "client_email": "whipsmart@whip-smart.iam.gserviceaccount.com",
-  "client_id": "104175171436754996858",
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/whipsmart%40whip-smart.iam.gserviceaccount.com"
-}''';
+      const cred = "";
 
 
-      const sId = '1Iy240k1B85VCQGsXlrzgKRslCDJi9ZtfzrLKRhXZAcE';
+      const sId = "";
 
       final gSheet = GSheets(cred);
       final ss = await gSheet.spreadsheet(sId);
